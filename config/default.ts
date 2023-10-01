5f1abbf9ccdc4b004cff68ae47a0e499e8885fea
KERIM Abdelmouiz <abdelmouizkerim@gmail.com>
2023-10-01 14:11:18 +0100

    Update default.ts

--- config/default.ts
+++ config/default.ts
@@ -1,4 +1,4 @@
 export default {
     port: 3000,
-    APIKEY: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiN2E0YzViZjYtMmM3NS00YjNhLTgwNDMtZGFkZWU3NWRlYzJiIiwidHlwZSI6ImFwaV90b2tlbiJ9.y7u5ID2xkOR9_X5n7EPvMsV_ZwtMUpTF1rtMwtLj_WY"
+    APIKEY: "Bearer <Your Token Here>"
   };
